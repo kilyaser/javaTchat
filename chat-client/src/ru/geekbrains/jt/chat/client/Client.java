435f9fad9450b99d01741e880c228fed4d36c545
Leva <piknik0711@yandex.ru>
2022-05-01 17:29:57 +0300

    added solution lesson 7 task 2

--- chat-client/src/ru/geekbrains/jt/chat/client/Client.java
+++ chat-client/src/ru/geekbrains/jt/chat/client/Client.java
@@ -22,8 +22,8 @@ public class Client extends JFrame implements ActionListener, Thread.UncaughtExc
     private final JTextField tfIPAddress = new JTextField("127.0.0.1");
     private final JTextField tfPort = new JTextField("8189");
     private final JCheckBox cbAlwaysOnTop = new JCheckBox("Always on top");
-    private final JTextField tfLogin = new JTextField("ivan_igorevich");
-    private final JPasswordField tfPassword = new JPasswordField("123456");
+    private final JTextField tfLogin = new JTextField("ivan-igorevich");
+    private final JPasswordField tfPassword = new JPasswordField("123");
     private final JButton btnLogin = new JButton("Login");
 
     private final JPanel panelBottom = new JPanel(new BorderLayout());
